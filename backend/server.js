04825ec69431cc4df890f5fd87b1ff873c2e7e6b
Lucas Saraiva <lucassaraivaxl@gmail.com>
2024-09-16 21:56:14 -0300

    Correção no server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const session = require('express-session'); 
-const MongoStore = require('connect-mongo'); // Adiciona o store para sessões no MongoDB
-const { authenticateToken } = require('./middleware'); 
+const { authenticateToken } = require('./middleware'); // Importar o middleware JWT
 
 // Importar as rotas
 const userRoutes = require('./routes/userRoutes');
@@ -14,26 +12,6 @@ const carRoutes = require('./routes/carRoutes');
 // Configuração do app
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Conectar ao MongoDB
-const mongoUri = 'mongodb+srv://lucasps6saraiva:vEZ8IrKk15orPlHV@nuvem.ayeyy.mongodb.net/?retryWrites=true&w=majority&appName=Nuvem';
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB conectado'))
-    .catch(err => console.error('Erro ao conectar ao MongoDB:', err.message));
-
-// Middleware de sessão com MongoDB como armazenamento
-app.use(session({
-  secret: 'seu_segredo_seguro', 
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: mongoUri, // Usa o MongoDB como storage para sessões
-  }),
-  cookie: { 
-    secure: process.env.NODE_ENV === 'production', // Define o cookie como "secure" em produção
-    maxAge: 1000 * 60 * 60 * 24 // Sessão expira em 24 horas
-  }
-}));
 
 // Middleware
 app.use(bodyParser.json());
@@ -44,6 +22,12 @@ app.use(express.static(path.join(__dirname, '../frontend'), {
   extensions: ['js', 'css', 'png', 'jpg'], 
   index: false // Desativa a capacidade de servir index.html automaticamente
 }));
+
+// Conectar ao MongoDB
+const mongoUri = 'mongodb+srv://lucasps6saraiva:vEZ8IrKk15orPlHV@nuvem.ayeyy.mongodb.net/?retryWrites=true&w=majority&appName=Nuvem';
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB conectado'))
+    .catch(err => console.error('Erro ao conectar ao MongoDB:', err.message));
 
 // Rotas protegidas para servir os arquivos HTML (addCar.html, addUser.html)
 app.get('/addCar', authenticateToken, (req, res) => {
@@ -63,16 +47,6 @@ app.get('/', (req, res) => {
 app.use('/users', userRoutes);
 app.use('/cars', carRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Rota para fazer logoff
-app.get('/logoff', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).send('Erro ao fazer logoff.');
-        }
-        res.redirect('/'); 
-    });
-});
 
 // Iniciar o servidor
 app.listen(port, () => {
