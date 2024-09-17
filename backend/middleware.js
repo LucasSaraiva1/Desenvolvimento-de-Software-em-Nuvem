7d88d833b00e83f4cde02e049f3310bbbe59ff48
Lucas Saraiva <lucassaraivaxl@gmail.com>
2024-09-16 22:07:56 -0300

    middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,12 +1,27 @@
-// middleware.js
+const jwt = require('jsonwebtoken');
+const secretKey = 'seu_segredo_seguro'; // Coloque sua chave secreta aqui
+
 const authenticateToken = (req, res, next) => {
-    // Supondo que você esteja usando sessão para gerenciar a autenticação
-    if (req.session && req.session.userId) {
-        // Se o usuário estiver autenticado, prossiga
-        return next();
+    // Obter o token do cabeçalho 'Authorization'
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        // Se não houver token, redireciona para a página de login
+        return res.redirect('/');
     }
-    // Se não estiver autenticado, redireciona para a página de login
-    return res.redirect('/');
+
+    // Verificar o token
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+            // Se o token for inválido ou expirado
+            return res.redirect('/');
+        }
+
+        // Se o token for válido, anexamos os dados do usuário ao req
+        req.user = user;
+        next(); // Prosseguir para a próxima função de middleware
+    });
 };
 
 module.exports = { authenticateToken };
